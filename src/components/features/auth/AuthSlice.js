51795c02fb531b0322b81aa7e01816e0be7fa422
Kibria934 <kibriahossain725@gmai.com>
2023-01-01 21:50:26 +0600

    redux setup

---
+++ src/components/features/auth/AuthSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  email: "",
+  role: "",
+  isLoading: false,
+  isError: false,
+  error: "",
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+});
